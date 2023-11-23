32e33e48b7d79c1d1891b043baa2084e9cdfc4a7
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2023-11-23 14:56:43 +0500

    Work Section 4th P

--- src/components/Work.js
+++ src/components/Work.js
@@ -85,6 +85,43 @@ const Work = () => {
               </div>
             </div>
           </motion.div>
+          <motion.div 
+                       variants={fadeIn('left', 0.2)}
+                       initial='hidden'
+                       whileInView={"show"}
+                       viewport={{ once: false, amount:0.3}}
+          className='flex-1 flex flex-col gap-y-10'>
+            {/* img */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className='group-hover:scale-125 transaction-all dutation-500' src={Img2} alt="" />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 '>
+                <span className='text-gradient'>Next Js Dashboard </span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50 '>
+                <span className='text-3xl text-white'>Acme Website</span>
+              </div>
+            </div>
+            {/* img */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className='group-hover:scale-125 transaction-all dutation-500' src={Img3} alt="" />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 '>
+                <span className='text-gradient'>ABC Design</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50 '>
+                <span className='text-3xl text-white'>Lorem Isppum  Website</span>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
