57072480bcdeff5398c23b98bf6e341a1f6a0e91
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-05-18 23:17:17 +0500

    refactor: improve header layout and button alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,23 @@ import Logo from '../assets/logo.png';
 const Header = () => {
   return <header className='py-8'>
       <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
+        <div className='flex items-center justify-between'>
           {/* logo */}
           <a href="syedabbasali1998.aa@gmail.com">
             <img src={Logo} alt="" />
           </a>
-          <a href="https://drive.google.com/file/d/1nsiGSJVEbt4AmI7vAE6NaieCwGQ7OKJg/view?usp=drivesdk">
-            <button className='btn btn-sm'>
-              See my Resume/CV 
-            </button>
-          </a>
-          <a>
-            <button className='btn btn-sm'>
-              Work With Me
-            </button>
-          </a>
+          <div>  
+            <a href="https://drive.google.com/file/d/1nsiGSJVEbt4AmI7vAE6NaieCwGQ7OKJg/view?usp=drivesdk">
+              <button className='ml-10 btn btn-sm'>
+                See my Resume/CV 
+              </button>
+            </a>
+            <a className="px-2">
+              <button className='btn btn-sm'>
+                Work With Me
+              </button>
+            </a>
+          </div>
         </div>
       </div>
   </header>;
